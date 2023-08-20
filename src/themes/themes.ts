4ce96bb8de64d0492f696ea01e614091142998b8
Arbaaz Mowlabucus <91611231+arbxz@users.noreply.github.com>
2023-08-20 21:11:22 +0400

    Compilation hotfixes

---
+++ src/themes/themes.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const AboutSection = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
