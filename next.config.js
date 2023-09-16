ef837b91066f093190e950fd98fe64d13395ccc2
Arbaaz Mowlabucus <91611231+arbxz@users.noreply.github.com>
2023-09-16 23:14:25 +0400

    styled components render compilation fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // i18n: {
+  //   locales: ["default", "en", "fr"],
+  //   defaultLocale: "default",
+  //   localeDetection: false,
+  //   domains: [
+  //     {
+  //       domain: "arbxz.com",
+  //       defaultLocale: "en",
+  //     },
+  //   ],
+  // },
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  trailingSlash: true,
+};
 
 module.exports = nextConfig;
