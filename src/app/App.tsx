97eddd2482e77cd238a14b9186f09c1ad383fc2e
Arbaaz Mowlabucus <91611231+arbxz@users.noreply.github.com>
2023-11-20 21:53:51 +0400

    Fix bg image on safari (#37)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { ThemeProvider } from "next-themes";
 
 import ArtSection from "./components/art-section/ArtSection";
@@ -9,13 +10,24 @@ import Modal from "./components/modal/Modal";
 import ProjectSection from "./components/project-section/ProjectSection";
 import SocialList from "./components/social-list/SocialList";
 import { useModalContext } from "./context/modal";
+import WebsiteBg from "/public/quantum-gradient.png";
 
 const App = () => {
   const { isModalOpen, modalContent } = useModalContext();
 
   return (
     <ThemeProvider>
-      <main className="relative bg-body text-foreground flex min-h-screen flex-col items-center justify-start select-none text-">
+      <main className="relative text-foreground flex min-h-screen flex-col items-center justify-start select-none">
+        <div className="fixed w-full h-screen object-cover top-0 left-0 -z-0">
+          <Image
+            src={WebsiteBg}
+            fill
+            placeholder="blur"
+            priority
+            alt="Website background"
+          />
+        </div>
+
         <MainBanner />
         <ProjectSection />
         <Contact />
