acf698fe6955c71b77c9a401790d0e771e0ae877
Arbaaz Mowlabucus <91611231+arbxz@users.noreply.github.com>
2023-11-24 07:39:18 +0400

    Added new projects

--- src/app/components/project-section/ProjectSection.tsx
+++ src/app/components/project-section/ProjectSection.tsx
@@ -3,7 +3,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 
-import sample from "/public/sample3.png";
+import cocagneImg from "/public/cocagne.jpeg";
+import jeanPigeonImg from "/public/sample3.png";
 
 interface InterfaceProjectItem {
   src: StaticImageData;
@@ -16,10 +17,18 @@ interface InterfaceProjectItem {
 const ProjectSection = () => {
   const projectList: InterfaceProjectItem[] = [
     {
-      src: sample,
+      src: jeanPigeonImg,
       url: "https://www.jeanpigeon.xyz/",
       name: "Jean-Pigeon",
       description: "A small website I co-authored with a friend.",
+      tech: "NextJS",
+    },
+    {
+      src: cocagneImg,
+      url: "https://www.cocagne.fr/",
+      name: "Cocagne.fr",
+      description:
+        "I implemented the frontend of this website within a team using NextJs and tailwind",
       tech: "NextJS",
     },
   ];
@@ -29,17 +38,17 @@ const ProjectSection = () => {
       <div
         className="relative bg-white flex card w-full sm:w-64 min-h-64 sm:flex-col flex-row rounded-md shadow-md ease-in-out transition-all duration-300 hover:shadow-lg hover:-translate-y-3 overflow-hidden"
         key={project.name}>
-        <div className="card-header">
+        <div className="w-2/5 h-auto sm:w-64 sm:h-44 object-cover object-bottom overflow-hidden">
           <Image
-            className=" object-center object-cover"
+            className="object-cover w-full h-full"
             src={project.src}
-            width={256}
             height={256}
+            width={256}
             alt={project.name}
             placeholder="blur"
           />
         </div>
-        <div className="card-body p-4 text-primary text-left">
+        <div className="card-body w-3/5 sm:w-full p-4 text-primary text-left">
           <span className="block text-xl font-semibold md:mb-2">
             {project.name}
           </span>
