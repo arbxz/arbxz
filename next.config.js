76821b469c867a4865b428ed47095af78d4c60aa
Arbaaz Mowlabucus <91611231+arbxz@users.noreply.github.com>
2023-12-05 15:10:08 +0400

    potential fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // i18n: {
-  //   locales: ["default", "en", "fr"],
-  //   defaultLocale: "default",
-  //   localeDetection: false,
-  //   domains: [
-  //     {
-  //       domain: "arbxz.com",
-  //       defaultLocale: "en",
-  //     },
-  //   ],
-  // },
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
