aebfc017b735c0a9c23b0e0558fb03bdc2e7c363
Arbaaz Mowlabucus <91611231+arbxz@users.noreply.github.com>
2023-12-05 15:21:47 +0400

    Added width to svg

--- src/app/components/graphic-section/GraphicSection.tsx
+++ src/app/components/graphic-section/GraphicSection.tsx
@@ -2,8 +2,8 @@ import GameboySVG from "../svg/Gameboy";
 
 const GraphicSection = () => {
   return (
-    <div className="flex items-center h-screen w-full py-5">
-      <div className="relative flex md:h-4/5 h-full w-auto mx-auto">
+    <div className="flex items-center py-5">
+      <div className="relative flex h-full w-[300px] mx-auto">
         <GameboySVG color="#4B4D52" colorShade="#333333" />
       </div>
     </div>
