16b23cc8681b6ce18fed46eab2ab88ac05e91a77
Arbaaz Mowlabucus <91611231+arbxz@users.noreply.github.com>
2024-01-16 14:49:33 +0400

    nextconfig

--- next.config.js
+++ next.config.js
@@ -2,16 +2,24 @@
  * @type {import('next').NextConfig}
  */
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
 
-const nextConfig = withBundleAnalyzer({
+// const nextConfig = withBundleAnalyzer({
+//   reactStrictMode: true,
+//   compiler: {
+//     styledComponents: true,
+//   },
+//   trailingSlash: true,
+// });
+
+const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
   trailingSlash: true,
-});
+};
 
 module.exports = nextConfig;
