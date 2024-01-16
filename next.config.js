3ade401104fa25561103556a75712b734e915865
Arbaaz Mowlabucus <91611231+arbxz@users.noreply.github.com>
2024-01-16 14:55:41 +0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
-
-// const withBundleAnalyzer = require("@next/bundle-analyzer")({
-//   enabled: process.env.ANALYZE === "true",
-// });
-
-// const nextConfig = withBundleAnalyzer({
-//   reactStrictMode: true,
-//   compiler: {
-//     styledComponents: true,
-//   },
-//   trailingSlash: true,
-// });
+/** @type {import('next').NextConfig} */
 
 const nextConfig = {
   reactStrictMode: true,
